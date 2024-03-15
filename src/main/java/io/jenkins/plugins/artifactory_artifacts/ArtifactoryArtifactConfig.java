f4b938a285de5ae34e5e4beb7419439b3f00266a
Valentin Delaye <jonesbusy@gmail.com>
2024-03-15 20:10:58 +0100

    Start adding ArtifactoryArtifactConfig

---
+++ src/main/java/io/jenkins/plugins/artifactory_artifacts/ArtifactoryArtifactConfig.java
@@ -0,0 +1,111 @@
+package io.jenkins.plugins.artifactory_artifacts;
+
+import com.cloudbees.plugins.credentials.CredentialsMatchers;
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import com.cloudbees.plugins.credentials.common.StandardListBoxModel;
+import com.cloudbees.plugins.credentials.common.StandardUsernameCredentials;
+import hudson.Extension;
+import hudson.ExtensionList;
+import hudson.model.AbstractDescribableImpl;
+import hudson.model.Descriptor;
+import hudson.model.Item;
+import hudson.security.ACL;
+import hudson.util.ListBoxModel;
+import java.io.Serializable;
+import java.util.Collections;
+import jenkins.model.Jenkins;
+import net.sf.json.JSONObject;
+import org.kohsuke.stapler.AncestorInPath;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
+import org.kohsuke.stapler.StaplerRequest;
+
+@Extension
+public class ArtifactoryArtifactConfig extends AbstractDescribableImpl<ArtifactoryArtifactConfig>
+        implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String storageCredentialId;
+    private String serverUrl;
+    private String repository;
+    private String prefix;
+
+    @DataBoundConstructor
+    public ArtifactoryArtifactConfig() {}
+
+    public String getStorageCredentialId() {
+        return storageCredentialId;
+    }
+
+    @DataBoundSetter
+    public void setStorageCredentialId(String storageCredentialId) {
+        this.storageCredentialId = storageCredentialId;
+    }
+
+    public String getServerUrl() {
+        return serverUrl;
+    }
+
+    @DataBoundSetter
+    public void setServerUrl(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+
+    public String getRepository() {
+        return repository;
+    }
+
+    @DataBoundSetter
+    public void setRepository(String repository) {
+        this.repository = repository;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    @DataBoundSetter
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public static ArtifactoryArtifactConfig get() {
+        return ExtensionList.lookupSingleton(ArtifactoryArtifactConfig.class);
+    }
+
+    @Extension
+    public static final class DescriptorImpl extends Descriptor<ArtifactoryArtifactConfig> {
+
+        public DescriptorImpl() {
+            load();
+        }
+
+        @Override
+        public boolean configure(StaplerRequest req, JSONObject json) throws FormException {
+            save();
+            return super.configure(req, json);
+        }
+
+        public ListBoxModel doFillStorageCredentialIdItems(@AncestorInPath Item item) {
+            StandardListBoxModel result = new StandardListBoxModel();
+            if (item == null) {
+                if (!Jenkins.get().hasPermission(Jenkins.ADMINISTER)) {
+                    return result.includeCurrentValue(get().getStorageCredentialId());
+                }
+            } else {
+                if (!item.hasPermission(Item.EXTENDED_READ) && !item.hasPermission(CredentialsProvider.USE_ITEM)) {
+                    return result.includeCurrentValue(get().getStorageCredentialId());
+                }
+            }
+            return result.includeEmptyValue()
+                    .includeMatchingAs(
+                            ACL.SYSTEM2,
+                            item,
+                            StandardUsernameCredentials.class,
+                            Collections.emptyList(),
+                            CredentialsMatchers.instanceOf(StandardUsernameCredentials.class))
+                    .includeCurrentValue(get().getStorageCredentialId());
+        }
+    }
+}
---
+++ src/test/java/io/jenkins/plugins/artifactory_artifacts/ConfigurationAsCodeTest.java
@@ -0,0 +1,19 @@
+package io.jenkins.plugins.artifactory_artifacts;
+
+import io.jenkins.plugins.casc.misc.ConfiguredWithCode;
+import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
+import io.jenkins.plugins.casc.misc.junit.jupiter.WithJenkinsConfiguredWithCode;
+import org.junit.jupiter.api.Test;
+
+@WithJenkinsConfiguredWithCode
+public class ConfigurationAsCodeTest {
+
+    @Test
+    @ConfiguredWithCode("configuration-as-code.yml")
+    public void shouldSupportConfigurationAsCode(JenkinsConfiguredWithCodeRule jenkinsRule) throws Exception {
+        ArtifactoryArtifactConfig.DescriptorImpl extension = jenkinsRule
+                .getInstance()
+                .getExtensionList(ArtifactoryArtifactConfig.DescriptorImpl.class)
+                .get(0);
+    }
+}
