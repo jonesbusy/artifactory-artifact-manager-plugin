86a5cd9cdc60eb640d22fe89eb0f8393630c47aa
Valentin Delaye <jonesbusy@gmail.com>
2024-03-15 20:56:35 +0100

    Complete JCasC test

--- src/test/java/io/jenkins/plugins/artifactory_artifacts/ConfigurationAsCodeTest.java
+++ src/test/java/io/jenkins/plugins/artifactory_artifacts/ConfigurationAsCodeTest.java
@@ -1,5 +1,8 @@
 package io.jenkins.plugins.artifactory_artifacts;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import io.jenkins.plugins.casc.misc.ConfiguredWithCode;
 import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
 import io.jenkins.plugins.casc.misc.junit.jupiter.WithJenkinsConfiguredWithCode;
@@ -15,5 +18,10 @@ public class ConfigurationAsCodeTest {
                 .getInstance()
                 .getExtensionList(ArtifactoryGenericArtifactConfig.DescriptorImpl.class)
                 .get(0);
+        ArtifactoryGenericArtifactConfig config = Utils.getArtifactConfig();
+        assertThat(config.getStorageCredentialId(), is("the-credentials-id"));
+        assertThat(config.getServerUrl(), is("http://localhost:7000"));
+        assertThat(config.getRepository(), is("my-generic-repo"));
+        assertThat(config.getPrefix(), is("jenkins/"));
     }
 }
