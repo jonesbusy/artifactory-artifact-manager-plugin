f5544affabbb19d1935d3107842f2f441059c765
Valentin Delaye <jonesbusy@gmail.com>
2024-03-15 21:50:21 +0100

    Remove stash for now

--- src/main/java/io/jenkins/plugins/artifactory_artifacts/ArtifactoryArtifactManager.java
+++ src/main/java/io/jenkins/plugins/artifactory_artifacts/ArtifactoryArtifactManager.java
@@ -1,23 +1,20 @@
 package io.jenkins.plugins.artifactory_artifacts;
 
 import edu.umd.cs.findbugs.annotations.NonNull;
-import hudson.EnvVars;
 import hudson.FilePath;
 import hudson.Launcher;
 import hudson.model.BuildListener;
 import hudson.model.Run;
-import hudson.model.TaskListener;
 import java.io.IOException;
 import java.util.Map;
 import java.util.logging.Logger;
 import jenkins.model.ArtifactManager;
 import jenkins.util.VirtualFile;
-import org.jenkinsci.plugins.workflow.flow.StashManager;
 import org.kohsuke.accmod.Restricted;
 import org.kohsuke.accmod.restrictions.NoExternalUse;
 
 @Restricted(NoExternalUse.class)
-public class ArtifactoryArtifactManager extends ArtifactManager implements StashManager.StashAwareArtifactManager {
+public class ArtifactoryArtifactManager extends ArtifactManager {
 
     private static final Logger LOGGER = Logger.getLogger(ArtifactManager.class.getName());
     private transient Run<?, ?> build;
@@ -45,33 +42,4 @@ public class ArtifactoryArtifactManager extends ArtifactManager implements Stash
     public VirtualFile root() {
         return new ArtifactoryVirtualFile(config.getRepository(), "artifacts", build);
     }
-
-    @Override
-    public void stash(
-            @NonNull String name,
-            @NonNull FilePath workspace,
-            @NonNull Launcher launcher,
-            @NonNull EnvVars env,
-            @NonNull TaskListener listener,
-            String includes,
-            String excludes,
-            boolean useDefaultExcludes,
-            boolean allowEmpty)
-            throws IOException, InterruptedException {}
-
-    @Override
-    public void unstash(
-            @NonNull String name,
-            @NonNull FilePath workspace,
-            @NonNull Launcher launcher,
-            @NonNull EnvVars env,
-            @NonNull TaskListener listener)
-            throws IOException, InterruptedException {}
-
-    @Override
-    public void clearAllStashes(@NonNull TaskListener listener) throws IOException, InterruptedException {}
-
-    @Override
-    public void copyAllArtifactsAndStashes(@NonNull Run<?, ?> to, @NonNull TaskListener listener)
-            throws IOException, InterruptedException {}
 }
