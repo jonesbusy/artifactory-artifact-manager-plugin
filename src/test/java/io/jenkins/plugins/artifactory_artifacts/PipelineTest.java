0fe5acfa73af12bb2a081109d3c1d9d0f80ce776
Andreas Hippler <andreas.hippler@goto.com>
2024-06-17 10:39:19 +0200

    test: improve tests: do archive and verify uploaded files

--- src/test/java/io/jenkins/plugins/artifactory_artifacts/PipelineTest.java
+++ src/test/java/io/jenkins/plugins/artifactory_artifacts/PipelineTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import hudson.model.Label;
@@ -26,16 +27,17 @@ public class PipelineTest extends BaseTest {
 
     @Test
     public void testPipelineWithPrefix(JenkinsRule jenkinsRule, WireMockRuntimeInfo wmRuntimeInfo) throws Exception {
-
         final String pipelineName = "testPipelineWithPrefix";
+        final String prefix = "jenkins/";
+        final String artifact = "artifact.txt";
 
-        configureConfig(jenkinsRule, wmRuntimeInfo, "jenkins/");
+        configureConfig(jenkinsRule, wmRuntimeInfo, prefix);
         String pipeline = IOUtils.toString(
                 Objects.requireNonNull(PipelineTest.class.getResourceAsStream("/pipelines/archiveController.groovy")),
                 StandardCharsets.UTF_8);
 
         // Setup wiremock stubs
-        setupWireMockStubs(pipelineName, wmRuntimeInfo, "jenkins/", "artifact.txt", "stash.tgz");
+        setupWireMockStubs(pipelineName, wmRuntimeInfo, prefix, artifact, "stash.tgz");
 
         // Run job
         WorkflowJob workflowJob = jenkinsRule.createProject(WorkflowJob.class, pipelineName);
@@ -45,6 +47,10 @@ public class PipelineTest extends BaseTest {
 
         // Job success
         assertThat(run1.getResult(), equalTo(hudson.model.Result.SUCCESS));
+
+        // Verify artifact was uploaded
+        WireMock.verify(WireMock.putRequestedFor(WireMock.urlEqualTo(getArtifactUrl(prefix, pipelineName, artifact)))
+                .withRequestBody(WireMock.equalTo("Hello, World!")));
 
         // Check 1 artifact
         assertThat(run1.getArtifacts(), hasSize(1));
@@ -54,15 +60,17 @@ public class PipelineTest extends BaseTest {
     public void testPipelineWithSpaces(JenkinsRule jenkinsRule, WireMockRuntimeInfo wmRuntimeInfo) throws Exception {
 
         final String pipelineName = "test Pipeline With spaces";
+        final String prefix = "jenkins artifacts/";
+        final String artifact = "my artifact.txt";
 
-        configureConfig(jenkinsRule, wmRuntimeInfo, "jenkins artifacts/");
+        configureConfig(jenkinsRule, wmRuntimeInfo, prefix);
         String pipeline = IOUtils.toString(
                 Objects.requireNonNull(
                         PipelineTest.class.getResourceAsStream("/pipelines/archiveControllerWithSpaces.groovy")),
                 StandardCharsets.UTF_8);
 
         // Setup wiremock stubs
-        setupWireMockStubs(pipelineName, wmRuntimeInfo, "jenkins artifacts/", "my artifact.txt", "my stash.tgz");
+        setupWireMockStubs(pipelineName, wmRuntimeInfo, prefix, artifact, "my stash.tgz");
 
         // Run job
         WorkflowJob workflowJob = jenkinsRule.createProject(WorkflowJob.class, pipelineName);
@@ -72,6 +80,10 @@ public class PipelineTest extends BaseTest {
 
         // Job success
         assertThat(run1.getResult(), equalTo(hudson.model.Result.SUCCESS));
+
+        // Verify artifact was uploaded
+        WireMock.verify(WireMock.putRequestedFor(WireMock.urlEqualTo(getArtifactUrl(prefix, pipelineName, artifact)))
+                .withRequestBody(WireMock.equalTo("Hello, World!")));
 
         // Check 1 artifact
         assertThat(run1.getArtifacts(), hasSize(1));
@@ -81,14 +93,16 @@ public class PipelineTest extends BaseTest {
     public void testPipelineWithoutPrefix(JenkinsRule jenkinsRule, WireMockRuntimeInfo wmRuntimeInfo) throws Exception {
 
         final String pipelineName = "testPipelineWithoutPrefix";
+        final String prefix = "";
+        final String artifact = "artifact.txt";
 
-        configureConfig(jenkinsRule, wmRuntimeInfo, "");
+        configureConfig(jenkinsRule, wmRuntimeInfo, prefix);
         String pipeline = IOUtils.toString(
                 Objects.requireNonNull(PipelineTest.class.getResourceAsStream("/pipelines/archiveController.groovy")),
                 StandardCharsets.UTF_8);
 
         // Setup wiremock stubs
-        setupWireMockStubs(pipelineName, wmRuntimeInfo, "", "artifact.txt", "stash.tgz");
+        setupWireMockStubs(pipelineName, wmRuntimeInfo, prefix, artifact, "stash.tgz");
 
         // Run job
         WorkflowJob workflowJob = jenkinsRule.createProject(WorkflowJob.class, pipelineName);
@@ -98,6 +112,10 @@ public class PipelineTest extends BaseTest {
 
         // Job success
         assertThat(run1.getResult(), equalTo(hudson.model.Result.SUCCESS));
+
+        // Verify artifact was uploaded
+        WireMock.verify(WireMock.putRequestedFor(WireMock.urlEqualTo(getArtifactUrl(prefix, pipelineName, artifact)))
+                .withRequestBody(WireMock.equalTo("Hello, World!")));
 
         // Check 1 artifact
         assertThat(run1.getArtifacts(), hasSize(1));
@@ -108,8 +126,10 @@ public class PipelineTest extends BaseTest {
             throws Exception {
 
         final String pipelineName = "testPipelineWithPrefix";
+        final String prefix = "jenkins/";
+        final String artifact = "artifact.txt";
 
-        configureConfig(jenkinsRule, wmRuntimeInfo, "jenkins/");
+        configureConfig(jenkinsRule, wmRuntimeInfo, prefix);
         String pipeline = IOUtils.toString(
                 Objects.requireNonNull(PipelineTest.class.getResourceAsStream("/pipelines/archiveAgent.groovy")),
                 StandardCharsets.UTF_8);
@@ -117,7 +137,7 @@ public class PipelineTest extends BaseTest {
         DumbSlave s = jenkinsRule.createSlave(Label.get("agent"));
 
         // Setup wiremock stubs
-        setupWireMockStubs(pipelineName, wmRuntimeInfo, "jenkins/", "artifact.txt", "stash.tgz");
+        setupWireMockStubs(pipelineName, wmRuntimeInfo, prefix, artifact, "stash.tgz");
 
         // Run job
         WorkflowJob workflowJob = jenkinsRule.createProject(WorkflowJob.class, pipelineName);
@@ -127,6 +147,10 @@ public class PipelineTest extends BaseTest {
 
         // Job success
         assertThat(run1.getResult(), equalTo(hudson.model.Result.SUCCESS));
+
+        // Verify artifact was uploaded
+        WireMock.verify(WireMock.putRequestedFor(WireMock.urlEqualTo(getArtifactUrl(prefix, pipelineName, artifact)))
+                .withRequestBody(WireMock.equalTo("Hello, World!")));
 
         // Check 1 artifact
         assertThat(run1.getArtifacts(), hasSize(1));
@@ -164,5 +188,9 @@ public class PipelineTest extends BaseTest {
         // HtmlSelect stageSelect = replayPage.getFirstByXPath("//select[@name='Result']");
         // final HtmlOption resultOption = stageSelect.getOptionByText("Result");
         // stageSelect.setSelectedAttribute(resultOption, true);
+    }
+
+    private String getArtifactUrl(String prefix, String pipelineName, String artifact) {
+        return ("/my-generic-repo/" + prefix + pipelineName + "/1/artifacts/" + artifact).replaceAll(" ", "%20");
     }
 }
--- src/test/resources/pipelines/replay.groovy
+++ src/test/resources/pipelines/replay.groovy
@@ -6,27 +6,23 @@ pipeline {
     }
     stages {
         stage('Agent 1') {
-          agent {
-              label 'built-in'
-          }
-          steps {
-            script {
-              if (isUnix()) {
-                sh 'mkdir target && echo "Hello" > target/foo.txt'
-              } else {
-                bat 'mkdir target && echo "Hello" > target/foo.txt'
-              }
+            agent {
+                label 'built-in'
             }
-            stash(name: 'stash', includes: 'target/**')
-          }
+            steps {
+                dir('target') {
+                    writeFile file: 'foo.txt', text: 'Hello'
+                }
+                stash(name: 'stash', includes: 'target/**')
+            }
         }
         stage('Result') {
-          agent {
-              label 'built-in'
-          }
-          steps {
-            unstash(name: 'stash')
-          }
+            agent {
+                label 'built-in'
+            }
+            steps {
+                unstash(name: 'stash')
+            }
         }
     }
 }
